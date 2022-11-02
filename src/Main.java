80aba690412805217242bee84cce13de2c15702a
Paul Connolly <paulconn5@gmail.com>
2022-11-02 13:46:10 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
-public class Main {
+import java.util.Scanner;
+public class Main
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Full Name Program
+        Scanner in = new Scanner(System.in);
+        String firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
+        String lastName = SafeInput.getNonZeroLenString(in, "Enter your last name");
+        System.out.println("\nYour full name is: " + firstName + " " + lastName);
+
+        //Favorite Number Program
+        int favInt;
+        double favDouble;
+        favInt = SafeInput.getInt(in, "Enter your favorite int");
+        favDouble = SafeInput.getDouble(in, "Enter your favorite double");
+        System.out.println("Favorite int: " + favInt + "\nFavorite Double: " + favDouble);
+
+        //Date and Time of Birth Program
+        int year;
+        int month;
+        int day;
+        int hours;
+        int minutes;
+        year = SafeInput.getRangedInt(in, "Enter year: ", 1950, 2010);
+        month = SafeInput.getRangedInt(in, "Enter month: ", 1, 12);
+        day = switch (month) {
+            //31 days
+            case 1, 3, 5, 7, 8, 10, 12 -> SafeInput.getRangedInt(in, "Enter day: ", 1, 31);
+            //28 days
+            case 2 -> SafeInput.getRangedInt(in, "Enter day: ", 1, 28);
+            //30 days
+            default -> SafeInput.getRangedInt(in, "Enter day: ", 1, 30);
+        };
+        hours = SafeInput.getRangedInt(in, "Enter hours: ", 1, 24);
+        minutes = SafeInput.getRangedInt(in, "Enter minutes: ", 1, 59);
+        System.out.println("You were born on " +
+                month + "/" + day + "/" + year + ", at " + hours + ":"
+                + minutes);
+
+
+        //Check out at the $10 store program
+        double price;
+        double total = 0;
+        boolean isDone;
+        do {
+            price = SafeInput.getRangedDouble(in, "Enter the price", 0.50, 9.99);
+            isDone = SafeInput.getYNConfirm(in, "Continue shopping?");
+            total = total + price;
+        }while (!isDone);
+        System.out.println("Your total is: " + total);
+
+
+        //RegEx is magic program;
+        String SSN;
+        String ucID;
+        String menuChoice;
+        SSN = SafeInput.getRegExString(in, "Enter the regex", "[0-9]{3}-[0-9]{2}-[0-9]{4}");
+        ucID = SafeInput.getRegExString(in, "Enter the regex", "[M|m][0-9]{5}");
+        menuChoice = SafeInput.getRegExString(in, "Enter the regex", "[OoSsVvQq]");
+        System.out.println("SSN: " + SSN + "\nUC ID: " + ucID + "\nMenu: " + menuChoice);
+
+        //Pretty header program;
+        SafeInput.prettyHeader("My amazing super message");
     }
+
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,2 +1,154 @@
-package PACKAGE_NAME;public class SafeInput {
+import java.util.Scanner;
+
+public class SafeInput {
+    /**
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt) {
+        String retString;  // Set this to zero length. Loop runs until it isn’t
+        do
+        {
+            System.out.print("\n" + prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        }while(retString.length() == 0);
+
+        return retString;
+
+    }
+
+    public static int getInt(Scanner pipe, String prompt) {
+        boolean done = false;
+        int retInt = 0;
+        do {
+            System.out.print("\n" + prompt + ": "); // show prompt add space
+            if(pipe.hasNextInt()) {
+                retInt = pipe.nextInt();
+                done = true;
+            } else {
+                System.out.println("You must enter a integer.");
+            }
+            pipe.nextLine();
+        }while(!done);
+        return retInt;
+    }
+
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high) {
+        boolean done = false;
+        int retInt = 0;
+        do {
+            System.out.print("\n" + prompt + low + "-" + high + ": "); // show prompt add space
+            if(pipe.hasNextInt()) {
+                retInt = pipe.nextInt();
+                if (retInt >= low && retInt <= high) {
+                    done = true;
+                }
+                else {
+                    System.out.println("You must enter an integer between " + low + "-" + high);
+                }
+            } else {
+                System.out.println("You must enter an integer.");
+            }
+            pipe.nextLine();
+        }while(!done);
+        return retInt;
+
+    }
+
+    public static double getDouble(Scanner pipe, String prompt) {
+        boolean done = false;
+        double retDouble = 0;
+        do {
+            System.out.print("\n" + prompt + ": "); // show prompt add space
+            if(pipe.hasNextDouble()) {
+                retDouble = pipe.nextDouble();
+                done = true;
+            } else {
+                System.out.println("You must enter a double.");
+            }
+            pipe.nextLine();
+        }while(!done);
+        return retDouble;
+    }
+
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high) {
+        boolean done = false;
+        double retDouble = 0;
+        do {
+            System.out.print("\n" + prompt + ": " + low + "-" + high + ": "); // show prompt add space
+            if(pipe.hasNextDouble()) {
+                retDouble = pipe.nextDouble();
+                if (retDouble >= low && retDouble <= high) {
+                    done = true;
+                }
+                else {
+                    System.out.println("You must enter a double between " + low + "-" + high);
+                }
+            } else {
+                System.out.println("You must enter a double.");
+            }
+            pipe.nextLine();
+        }while(!done);
+        return retDouble;
+    }
+
+    public static boolean getYNConfirm(Scanner pipe, String prompt) {
+        boolean complete = false;
+        boolean retBool = false;
+        String retString;
+        do {
+           System.out.print("\n" + prompt + ": "); // show prompt add space
+           retString = pipe.nextLine();
+           if (retString.equalsIgnoreCase("Y")) {
+               complete = true;
+           }
+           else if (retString.equalsIgnoreCase("N")) {
+               complete = true;
+               retBool = true;
+           }
+           else {
+               System.out.println("Enter [Y/N]");
+           }
+       }while(!complete);
+    return retBool;
+    }
+
+    public static String getRegExString(Scanner pipe, String prompt, String regEx) {
+        boolean done = false;
+        String retString;
+        do {
+            System.out.print("\n" + prompt + " " + regEx + ": "); // show prompt add space
+            retString = pipe.nextLine();
+            if (retString.matches(regEx)) {
+                done = true;
+            }
+            else {System.out.println("You must follow the regex pattern!");}
+        } while(!done);
+
+        return retString;
+
+    }
+
+    //Pretty Header Program
+    public static void prettyHeader(String msg) {
+        for (int i=1; i<61; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n***");
+        for (double s = 0.5*(54-msg.length()); s>0; s--) {
+            System.out.print(" ");
+        }
+        System.out.print(msg);
+        for (double s = 0.5*(54-msg.length()); s>0; s--) {
+            System.out.print(" ");
+        }
+        System.out.print("***\n");
+        for (int i=1; i<61; i++) {
+            System.out.print("*");
+        }
+    }
 }
+
+
